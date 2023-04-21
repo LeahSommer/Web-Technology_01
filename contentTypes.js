efc0992002b9e13a63e903828f1ef53a8d4cbff0
LeahSommer <sommer.leah@googlemail.com>
2023-04-21 14:37:16 +0200

    html seite eingefügt

---
+++ contentTypes.js
@@ -0,0 +1,11 @@
+module.exports = {
+    html: {
+    "Content-Type": "text/html"
+    },
+    text: {
+    "Content-Type": "text/plain"
+    },
+    js: {
+    "Content-Type": "text/js"
+    }
+   };
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const port = 3000,
+/*const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
   app = http.createServer((request, response) => {
@@ -13,4 +13,19 @@ const port = 3000,
   });
 app.listen(port);
 console.log(`The server has started and is listening on port number:
-➥ ${port}`);
+➥ ${port}`);*/
+const port = 3000,
+ http = require("http"),
+ httpStatus = require("http-status-codes"),
+ router = require("./router"),
+ contentTypes = require("./contentTypes"),
+ utils = require("./utils");
+ 
+ router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.htm);
+  utils.getFile("views/index.html", res);
+ });
+ 
+http.createServer(router.handle).listen(port);
+console.log(`The server is listening on
+➥ port number: ${port}`);
